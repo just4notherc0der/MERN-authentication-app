a1b1a7e0175ac51c08437c0d72bbafee689d6f6c
just4notherc0der <just4notherc0der@gmail.com>
2017-08-31 14:42:37 +0200

    hot reloading implemented

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@ import express from 'express';
 import path from 'path';
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev';
 
 let app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+const compiler = webpack(webpackConfig);
+
+app.use(webpackMiddleware(compiler, {
+  hot: true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
@@ -18,5 +26,12 @@ app.listen(8080, () => console.log('Application started on loclhost:8080'));
 
 Webpack middleware takes a compiler, and a compiler is the webpack function, which
 needs configuration (webpackConfig) to run.
+
+For hot reloading to work, we use another middleware for express:
+If express is not used in the project, than webpack-dev-server is right choice
+
+We also need to install and setup react-hot loader to teach react to reload itself
+For hot-reloading to work, components must be class based, not functional
+Functional components can still be used, but not as most top-level component
 
 */
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,17 +3,25 @@ import webpack from 'webpack';
 
 export default {
   devtools: 'eval-source-map',
-  entry: path.join(__dirname, '/client/index.js'),
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
   output: {
     path: '/',
     publicPath: '/'
   },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.js$/,
         include: path.join(__dirname, 'client'),
-        loaders: ['babel']
+        loaders: ['react-hot', 'babel']
       }
     ]
   },
@@ -26,5 +34,8 @@ export default {
 
 Instead of actually creating bundle.js file, middleware will serve this from memory.
 Only include js files from client folder.
+
+new webpack.NoErrorsPlugin() - creates cleaner error messages
+new webpack.optimize.OccurenceOrderPlugin() - ensures consistent build hashes
 
 */
